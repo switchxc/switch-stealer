7969562e0e5f8378003a94f08a5d489e52b57e36
quack quack <137829849+kryyyaaaa@users.noreply.github.com>
2025-02-07 23:13:28 +0300

    Add files via upload

--- peredoz/build.bat
+++ peredoz/build.bat
@@ -16,5 +16,60 @@ if %errorlevel% neq 0 (
 echo .NET SDK is installed. Proceeding to publish the project...
 dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true peredoz.sln
 
+:: Создаем папку build, если её нет
+set "BUILD_FOLDER=%~dp0build"
+if not exist "%BUILD_FOLDER%" (
+    mkdir "%BUILD_FOLDER%"
+    if %errorlevel% neq 0 (
+        echo Failed to create the build folder.
+        pause
+        exit /b
+    )
+
+)
+
+:: Переносим exe-файл в папку build
+set "EXE_SOURCE=bin\Release\net8.0\win-x64\publish\peredoz.exe"
+set "EXE_DESTINATION=%BUILD_FOLDER%\peredoz.exe"
+
+if exist "%EXE_SOURCE%" (
+    move /Y "%EXE_SOURCE%" "%EXE_DESTINATION%"
+    if %errorlevel% neq 0 (
+        echo Failed to move the executable file.
+        pause
+        exit /b
+    )
+    
+) else (
+    echo Executable file not found in the expected location.
+    pause
+    exit /b
+)
+
+:: Удаляем папки bin и obj
+if exist "bin" (
+    rmdir /S /Q "bin"
+    if %errorlevel% neq 0 (
+        echo Failed to delete the bin folder.
+        pause
+        exit /b
+    )
+    
+)
+
+if exist "obj" (
+    rmdir /S /Q "obj"
+    if %errorlevel% neq 0 (
+        echo Failed to delete the obj folder.
+        pause
+        exit /b
+    )
+    
+)
+
+echo.
+echo stealer in "build" folder.
+echo.
+
 pause
 exit
